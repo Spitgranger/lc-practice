934e0be39ae32ea839d821803518f1ce8a1b01c4
spitgranger <richard.fan2000@hotmail.com>
2023-07-17 23:53:21 -0400

    initial draft for longest consecutive sequence

---
+++ 128_longest_consecutive_sequence.py
@@ -0,0 +1,25 @@
+# Problem description: Given an unsorted array of integers nums, return
+# The length of the longest consecutive elements sequence. Algorithm must
+# run in O(n) time
+# My initial thoughts: We can sort the array and then find the sequences that way
+# however that would mean O(nlogn) complexity.
+
+
+# TODO my first implmentation. Naive and doesn't work all the time
+def longest_consecutive(nums):
+    sorted_nums = sorted(nums)
+    longest = 0
+    current = 0
+    for i in range(len(sorted_nums)):
+        if i == 0:
+            current += 1
+            longest = current
+        else:
+            if sorted_nums[i - 1] + 1 == sorted_nums[i]:
+                current += 1
+            if longest <= current:
+                longest = current
+            else:
+                current = 0
+
+    return longest
