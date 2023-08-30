6c59fcd6af0d5eb6a2156c747aa2b6ec0411c3a0
spitgranger <richard.fan2000@hotmail.com>
2023-08-30 17:12:09 -0400

    added solution for 572 subtree of another tree

---
+++ trees/572_subtree_of_another_tree.py
@@ -0,0 +1,22 @@
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+def isSubtree(root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+    def equal(tree1, tree2):
+        if not tree1 and not tree2:
+            return True
+        if not tree1 and tree2 or tree1 and not tree2:
+            return False
+        if tree1.val != tree2.val:
+            return False
+        return equal(tree1.left, tree2.left) and equal(tree1.right, tree2.right)
+    def dfs(r):
+        if not r:
+            return False
+        if equal(r, subRoot):
+            return True
+        return dfs(r.right) or dfs(r.left)
+    return dfs(root)
