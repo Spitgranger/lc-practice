f9554394e09aad246d460bfcf34e90a6619f3b15
spitgranger <richard.fan2000@hotmail.com>
2023-09-07 13:13:26 -0400

    added solution for 200 number of islands

---
+++ graphs/200_number_of_islands.py
@@ -0,0 +1,20 @@
+from typing import List
+# This is a O(n * m) solution. The idea is exactly the same as finding all connected components in a graph.
+# Run dfs on every number that is not 0. If we haven't visited it, increment the number of islands by one.
+def numIslands(grid: List[List[str]]) -> int:
+    visited = set()
+    number = 0
+    def dfs(i, j):
+        if i >= len(grid) or j >= len(grid[0]) or i < 0 or j < 0 or (i, j) in visited or grid[i][j] == '0':
+            return
+        visited.add((i, j))
+        dfs(i + 1, j)
+        dfs(i, j + 1)
+        dfs(i - 1, j)
+        dfs(i, j - 1)
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if (i, j) not in visited and grid[i][j] == '1':
+                number += 1
+            dfs(i, j)
+    return number
