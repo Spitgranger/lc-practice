22c4e468eb2c681962a3d42993a82949c385a189
spitgranger <richard.fan2000@hotmail.com>
2023-12-28 23:31:13 -0500

    added solution for 1768 merge strings alternatively

---
+++ lc75/1768_merge_strings_alternatively.py
@@ -0,0 +1,11 @@
+def mergeAlternately(word1: str, word2: str) -> str:
+    ret = ""
+    for i in range(max(len(word1), len(word2))):
+        if i < len(word1):
+            ret += word1[i]
+        if i < len(word2):
+            ret += word2[i]
+    return ret
+
+
+print(mergeAlternately("ab", "pqrs"))
